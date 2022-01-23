ecde22804b70261df346f2a59f92d26ca889394c
gdivakov <g.divakov@weplay.tv>
2022-01-23 16:55:00 +0200

    updated related to new leaflet api

--- client/src/data/layers.js
+++ client/src/data/layers.js
@@ -1,100 +1,61 @@
 import L from 'leaflet';
 
-const mapboxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+const mapboxUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
 const attribution = '';
 const accessToken = 'pk.eyJ1IjoiamFja3NtZWV0IiwiYSI6ImNqamw4azNzYzFhbzEzdm0wZjZ2MWw4OGsifQ.asLd4d0r0Id6VlYIwth4Pg';
 
 const Light = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.light',
-	attribution,
+	tileSize: 512,
 	maxZoom: 18,
-	accessToken
+	zoomOffset: -1,
+	id: 'mapbox/light-v10',
+	accessToken,
+	attribution,
 });
 const Streets = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.streets',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-const Wheatpaste = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.wheatpaste',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const Satellite = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.satellite',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const StreetsSatellite = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.streets-satellite',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const StreetsBasic = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.streets-basic',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const Comic = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.comic',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const Outdoors = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.outdoors',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const RunBikeHike = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.run-bike-hike',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const Pencil = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.pencil',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const Pirates = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.pirates',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const Emerald = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.emerald',
-	attribution,
-	maxZoom: 18,
-	accessToken
-});
-
-const HighContrast = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.high-contrast',
+	id: 'mapbox/streets-v11',
 	attribution,
 	maxZoom: 18,
 	accessToken
 });
 
 const Dark = L.tileLayer(mapboxUrl, {
-	id: 'mapbox.dark',
+	id: 'mapbox/dark-v10',
+	attribution,
+	maxZoom: 18,
+	accessToken
+});
+
+const Satellite = L.tileLayer(mapboxUrl, {
+	id: 'mapbox/satellite-v9',
+	attribution,
+	maxZoom: 18,
+	accessToken
+});
+
+const StreetsSatellite = L.tileLayer(mapboxUrl, {
+	id: 'mapbox/satellite-streets-v11',
+	attribution,
+	maxZoom: 18,
+	accessToken
+});
+
+const NavigationDay = L.tileLayer(mapboxUrl, {
+	id: 'mapbox/navigation-day-v1',
+	attribution,
+	maxZoom: 18,
+	accessToken
+});
+
+const Outdoors = L.tileLayer(mapboxUrl, {
+	id: 'mapbox/outdoors-v11',
+	attribution,
+	maxZoom: 18,
+	accessToken
+});
+
+const NavigationNight = L.tileLayer(mapboxUrl, {
+	id: 'mapbox/navigation-night-v1',
 	attribution,
 	maxZoom: 18,
 	accessToken
@@ -103,16 +64,10 @@ const Dark = L.tileLayer(mapboxUrl, {
 export default {
 	Light,
 	Streets,
-	Wheatpaste,
+	Dark,
 	Satellite,
 	StreetsSatellite,
-	StreetsBasic,
-	Comic,
+	NavigationDay,
+	NavigationNight,
 	Outdoors,
-	RunBikeHike,
-	Pencil,
-	Pirates,
-	Emerald,
-	HighContrast,
-	Dark
 };
