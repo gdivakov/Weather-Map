26b8654dd6667fe1327caf175480469119c91829
gdivakov <g.divakov@weplay.tv>
2022-02-10 11:29:49 +0200

    additional improvements

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import MapApp from './MapApp'
 import WeatherTab from './WeatherTab'
 import BodyDrawer from './BodyDrawer'
@@ -7,12 +7,10 @@ import '../css/map.css'
 
 const App = () => {
 	return (
-		<Fragment>
-			<BodyDrawer>
-				<div position="relative" className="wrap"> <MapApp /> </div>
-				<WeatherTab />
-			</BodyDrawer>
-		</Fragment>
+		<BodyDrawer>
+			<div position="relative" className="wrap"> <MapApp /> </div>
+			<WeatherTab />
+		</BodyDrawer>
 	);
 }
 
